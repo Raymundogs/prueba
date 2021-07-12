3079717f016dced044d5cb7cddb91a12e06caec6
Raymundo Guaraguara <raymundogs@gmail.com>
2021-07-12 14:45:11 -0400

    test

--- src/main/java/CheckStyleApp.java
+++ src/main/java/CheckStyleApp.java
@@ -1,5 +1,21 @@
-public class CheckStyleApp {
-    public static void main(String[] args) {
+public final class CheckStyleApp {
+    private CheckStyleApp() {
+    }
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(final String[] args) {
+
         System.out.println("Hello, CheckStyle");
+    }
+
+    /**
+     *
+     * @param text
+     */
+    public void hello(final String text) {
+        System.out.println("xd");
     }
 }
